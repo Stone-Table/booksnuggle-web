a169035572b58f54f3da61d3135dd9dae3a21ad6
teelrabbit <xuntitled@protonmail.com>
2024-11-13 10:32:11 -0800

    adding support for github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: process.env.GITHUB_PAGES ? '/booksnuggle-web' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
