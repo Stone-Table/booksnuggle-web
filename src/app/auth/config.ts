80c342135687aa76404b73dd947ce458a5083d74
teelrabbit <xuntitled@protonmail.com>
2024-11-18 13:43:49 -0800

    fixing gmail login and adding defulat admin login

--- src/app/auth/config.ts
+++ src/app/auth/config.ts
@@ -1,8 +1,56 @@
 import type { AuthOptions } from 'next-auth'
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export function getAuthConfig(): AuthOptions {
   return {
-    // Add your auth providers and configuration here
-    providers: [],
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      }),
+      GithubProvider({
+        clientId: process.env.GITHUB_ID!,
+        clientSecret: process.env.GITHUB_SECRET!,
+      }),
+      CredentialsProvider({
+        name: 'Credentials',
+        credentials: {
+          username: { label: "Username", type: "text" },
+          password: { label: "Password", type: "password" }
+        },
+        async authorize(credentials) {
+          if (credentials?.username === "admin" && credentials?.password === "admin") {
+            return {
+              id: "1",
+              name: "Guest Admin",
+              email: "guest@example.com"
+            }
+          }
+          return null
+        }
+      }),
+    ],
+    pages: {
+      signIn: '/login',
+      error: '/login',
+    },
+    callbacks: {
+      async session({ session }) {
+        return session;
+      },
+      async jwt({ token }) {
+        return token;
+      },
+      async redirect({ url, baseUrl }) {
+        return '/mybooks';
+      }
+    },
+    session: {
+      strategy: "jwt",
+      maxAge: 24 * 60 * 60 // 24 hours
+    },
+    secret: process.env.NEXTAUTH_SECRET
   }
 } 
